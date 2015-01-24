c11d91b4f9a6e3e5ddbdcc204223ba2fd8290ff8
willle3456 <willle3456@gmail.com>
2015-01-23 21:33:22 -0800

    field name changes in character class. task package created.

---
+++ src/tasks/Task.java
@@ -0,0 +1,23 @@
+package tasks;
+
+public class Task {
+    //task name
+    private String name;
+    
+    //list of characters
+    private String[] characters;
+    
+    //skills needed
+    private int fighting;
+    private int scavenging;
+    private int engineering;
+    
+    //character effects
+    private int fear;
+    private int insanity;
+    private int boredom;
+    //pass fail
+    
+    //addCharacter
+
+}
