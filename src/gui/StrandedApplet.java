d69230925e9c9e341ea235a71f9a6395016c1955
Jono.chadwell <jono.chadwell@gmail.com>
2015-01-23 20:12:13 -0800

    Added comment for testing purposes

--- src/gui/StrandedApplet.java
+++ src/gui/StrandedApplet.java
@@ -3,6 +3,7 @@ package gui;
 import processing.core.*;
 
 public class StrandedApplet extends PApplet {
+    //This is my change
     public static void main(String[] args) {
         PApplet.main(new String[] { "--present", "MyProcessingSketch" });
     }
