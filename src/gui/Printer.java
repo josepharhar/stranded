4f665b55a05c37d6f22b72170fd3731b77e36771
Joseph Arhar <josepharhar@gmail.com>
2015-01-23 21:47:04 -0800

    Made fonts work

--- src/gui/Printer.java
+++ src/gui/Printer.java
@@ -1,22 +1,26 @@
 package gui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import processing.core.*;
 
-import gui.StrandedApplet.*;
-
 public class Printer {
-    private List<String> textQueue;
+    //The PApplet to draw to
     private StrandedApplet applet;
+    
+    private List<String> textQueue;
     private PFont font;
     
     public Printer(StrandedApplet applet) {
         this.applet = applet;
+        
         //Set up the text font and size once here
         //applet.textSize(32);
         font = applet.createFont("Monospaced.plain", 16);
         applet.textFont(font);
+        
+        textQueue = new ArrayList<String>();
     }
     
     public void print(String text) {
