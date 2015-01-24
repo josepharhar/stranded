433186974b0dc5f4049f95f78fba30469c2ec5f7
Joseph Arhar <josepharhar@gmail.com>
2015-01-23 21:32:13 -0800

    Added Printer

---
+++ src/gui/Printer.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.util.List;
+
+import processing.core.*;
+
+import gui.StrandedApplet.*;
+
+public class Printer {
+    private List<String> textQueue;
+    private StrandedApplet applet;
+    private PFont font;
+    
+    public Printer(StrandedApplet applet) {
+        this.applet = applet;
+        //Set up the text font and size once here
+        //applet.textSize(32);
+        font = applet.createFont("Monospaced.plain", 16);
+        applet.textFont(font);
+    }
+    
+    public void print(String text) {
+        if (textQueue.size() > 3) {
+            textQueue.remove(0);
+            textQueue.add(text);
+        }
+    }
+    
+    public void clear() {
+        textQueue.clear();
+    }
+    
+    /**
+     * To be called every frame
+     * @precondition The screen has been translated to the
+     *          correct location for the "textbox" on screen
+     */
+    public void draw() {
+        applet.text("asdf", 10, 10);
+    }
+}
--- src/gui/StrandedApplet.java
+++ src/gui/StrandedApplet.java
@@ -3,18 +3,27 @@ package gui;
 import processing.core.*;
 
 public class StrandedApplet extends PApplet {
-    //This is my change
-    public static void main(String[] args) {
-        PApplet.main(new String[] { "--present", "MyProcessingSketch" });
-    }
+    
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    private Printer printer;
     
     public void setup() {
-        size(800, 600);
+        printer = new Printer(this);
+        
+        //temp
+        printer.print("some text");
+        printer.print("more text");
+        
+        size(WIDTH, HEIGHT);
     }
     
     public void draw() {
         background(0);
         fill(255, 0, 0);
         ellipse(width / 2, height / 2, 50, 50);
+        
+        printer.draw();
     }
 }
