af7f0e69f7d0dc80e3cc77a0e4858c1105cffd5d
Joseph Arhar <josepharhar@gmail.com>
2015-01-23 19:57:29 -0800

    Initial Commit

---
+++ src/gui/StrandedApplet.java
@@ -0,0 +1,19 @@
+package gui;
+
+import processing.core.*;
+
+public class StrandedApplet extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main(new String[] { "--present", "MyProcessingSketch" });
+    }
+    
+    public void setup() {
+        size(800, 600);
+    }
+    
+    public void draw() {
+        background(0);
+        fill(255, 0, 0);
+        ellipse(width / 2, height / 2, 50, 50);
+    }
+}
