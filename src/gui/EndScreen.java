ef417c452027ef8e10f255676c0abe4567554782
Joseph Arhar <josepharhar@gmail.com>
2015-01-25 15:43:02 -0800

    added endgame win

--- src/gui/EndScreen.java
+++ src/gui/EndScreen.java
@@ -1,6 +1,11 @@
 package gui;
 
 import processing.core.*;
+import timing.GameTimer;
+import gui.StartScreen.Star;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class EndScreen {
     
@@ -8,12 +13,23 @@ public class EndScreen {
     
     private PImage[] escape;
     private PImage[] explode;
+    private PImage ship;
     
     private int currentFrame;
     
     private long startTime;
+    private long currentTime;
+    private double timeDiff;
+    
+    private List<Star> stars;
     
     public EndScreen(StrandedApplet applet) {
+        stars = new LinkedList<Star>();
+        
+        while (stars.size() < 200) {
+            stars.add(new Star(applet.random(0, applet.GAME_WIDTH), applet.random(0, applet.GAME_HEIGHT), applet.random(1f, 2f)));
+        }
+        
         this.applet = applet;
         
         escape = new PImage[8];
@@ -37,22 +53,29 @@ public class EndScreen {
         explode[7] = applet.loadImage("pictures/end/explode_8.png");
         explode[8] = applet.loadImage("pictures/end/explode_9.png");
         
+        ship = applet.loadImage("pictures/end/ship_big.png");
+        
         startTime = System.currentTimeMillis();
     }
     
     public void draw() {
-//        if (win) {
-//            drawWin();
-//        } else {
-//            drawLose();
-//        }
+        applet.background(0);
+        
+        currentTime = System.currentTimeMillis();
+        timeDiff = currentTime - startTime;
+        
+        drawStars();
+        
+        if (true) {
+            drawWin();
+        } else {
+            drawLose();
+        }
+        
     }
     
     private void drawWin() {
-        applet.background(0);
-        
-        long currentTime = System.currentTimeMillis();
-        double timeDiff = (currentTime - startTime) / 1000.0;
+        timeDiff = (currentTime - startTime) / 1000.0;
         
         if (timeDiff < 0.5) {
             currentFrame = 0;
@@ -72,10 +95,68 @@ public class EndScreen {
             currentFrame = 7;
         }
         
-        applet.image(escape[currentFrame], 400, 300);
+        if (timeDiff < 4.0) {
+            applet.image(escape[currentFrame], 400, 300);
+        } else {
+            applet.fill(0, 255, 0);
+            applet.textAlign(applet.CENTER, applet.CENTER);
+            applet.textSize(48);
+            applet.text("You Escaped!", applet.GAME_WIDTH / 2, applet.GAME_HEIGHT / 2 - 100);
+            applet.image(ship, 450, 300);
+            moveStars();
+        }
     }
     
     private void drawLose() {
         
+    }
+    
+    private void moveStars() {
+        // Remove stars
+        for (int i = stars.size() - 1; i >= 0; i--) {
+            Star star = stars.get(i);
+            if (star.loc.x < 0) {
+                stars.remove(i);
+            }
+        }
+        
+        // Add stars
+        if (Math.random() > 0.2) {
+            float x = applet.GAME_WIDTH + 100f;
+            float y = applet.random(0, applet.GAME_HEIGHT);
+            float r = applet.random(1f, 2f);
+            stars.add(new Star(x, y, r));
+        }
+        
+        // Move stars
+        for (Star star : stars) {
+            star.move();
+        }
+    }
+    
+    private void drawStars() {
+        applet.noStroke();
+        applet.fill(255);
+        for (Star star : stars) {
+            applet.ellipse(star.loc.x, star.loc.y, star.r * 2, star.r * 2);
+        }
+    }
+    
+    // Represents one of the stars
+    class Star {
+        public PVector loc;
+        public PVector velocity;
+        public float r;
+        
+        public Star(float x, float y, float r) {
+            loc = new PVector(x, y);
+            this.r = r;
+            velocity = new PVector((r-.2f) * (r-.2f) * -2, 0);
+        }
+        
+        public void move() {
+            //velocity.mult(1 + (r / 100));
+            loc.add(velocity);
+        }
     }
 }
