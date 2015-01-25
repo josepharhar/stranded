17f5bc0c8c1fc390a97199f62b622921b5e1ac90
Joseph Arhar <josepharhar@gmail.com>
2015-01-24 21:43:24 -0800

    fixed expiration date

--- src/gui/TaskDetail.java
+++ src/gui/TaskDetail.java
@@ -7,25 +7,27 @@ import main.Resource;
 import tasks.Task;
 
 public class TaskDetail extends SidebarItem {
-    
-    //The job that this detail is showing
+
+    // The job that this detail is showing
     private Task task;
-    
+
     private Button buttonAssign;
-    
+
     public TaskDetail(StrandedApplet applet, Game game, Task task) {
         super("Task Detail", applet, game);
         this.task = task;
-        buttonAssign = new Button(applet.SIDEBAR_WIDTH / 2 - 20, applet.SIDEBAR_HEIGHT - 80, 72, 32, applet.loadImage("pictures/assign.png"));
+        buttonAssign = new Button(applet.SIDEBAR_WIDTH / 2 - 20,
+                applet.SIDEBAR_HEIGHT - 80, 72, 32,
+                applet.loadImage("pictures/assign.png"));
     }
-    
+
     public void draw() {
         super.draw();
-        
+
         int x = 20;
         int y = 40;
-        
-        //Name
+
+        // Name
         applet.fill(0, 128, 0);
         applet.text("Task Name: ", x, y);
         y += 30;
@@ -33,7 +35,7 @@ public class TaskDetail extends SidebarItem {
         applet.text(" " + task.getName(), x, y);
         y += 30;
 
-        //Costs
+        // Costs
         applet.fill(0, 128, 0);
         applet.text("Costs: ", x, y);
         y += 30;
@@ -43,7 +45,7 @@ public class TaskDetail extends SidebarItem {
             y += 30;
         }
 
-        //Rewards
+        // Rewards
         applet.fill(0, 128, 0);
         applet.text("Rewards: ", x, y);
         y += 30;
@@ -52,45 +54,48 @@ public class TaskDetail extends SidebarItem {
             applet.text(" " + reward.getKey() + ": " + reward.getValue(), x, y);
             y += 30;
         }
-        
-        //Penalties
+
+        // Penalties
         applet.fill(0, 128, 0);
         applet.text("Penalties: ", x, y);
         y += 30;
         applet.fill(0, 255, 0);
         for (Map.Entry<Resource, Double> penalty : task.getPenalty().entrySet()) {
-            applet.text(" " + penalty.getKey() + ": " + penalty.getValue(), x, y);
+            applet.text(" " + penalty.getKey() + ": " + penalty.getValue(), x,
+                    y);
             y += 30;
         }
 
-        //Difficulty
+        // Difficulty
         applet.fill(0, 128, 0);
         applet.text("Difficulty: ", x, y);
         y += 30;
         applet.fill(0, 255, 0);
         applet.text(" " + task.getDifficulty(), x, y);
         y += 30;
-        
-        //Skill
+
+        // Skill
         applet.fill(0, 128, 0);
         applet.text("Primary Skill: ", x, y);
         y += 30;
         applet.fill(0, 255, 0);
         applet.text(" " + task.getPrimarySkill(), x, y);
         y += 30;
-        
-        //Expiration
-        applet.fill(0, 128, 0);
-        applet.text("Expiration: ", x, y);
-        y += 30;
-        applet.fill(0, 255, 0);
-        applet.text(" " + task.getExpirationTime(), x, y);
-        y += 30;
-        
-        //Assign Button
+
+        // Expiration
+        if (task.isExpires()) {
+            applet.fill(0, 128, 0);
+            applet.text("Expiration: ", x, y);
+            y += 30;
+            applet.fill(0, 255, 0);
+            applet.text(" " + ((task.getExpirationTime() - System.currentTimeMillis()) / 1000L) + " seconds", x, y);
+            y += 30;
+        }
+
+        // Assign Button
         buttonAssign.draw(applet);
     }
-    
+
     public void click(float mousex, float mousey) {
         super.click(mousex, mousey);
         if (buttonAssign.isClicked(mousex, mousey)) {
