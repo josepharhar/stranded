064f379f10b1dcc085523af8daabe55a5f1e0b07
Joseph Arhar <josepharhar@gmail.com>
2015-01-25 21:03:13 -0800

    added resources/ to audio files

--- src/audio/Audio.java
+++ src/audio/Audio.java
@@ -37,37 +37,37 @@ public class Audio {
     }
     
     public void updateBeep() {
-        SamplePlayer beep = new SamplePlayer(ac, SampleManager.sample("Status_Update.wav"));
+        SamplePlayer beep = new SamplePlayer(ac, SampleManager.sample("resources/Status_Update.wav"));
         ac.out.addInput(beep);
     }
     
     public void sideBeep() {
-        SamplePlayer Sbeep = new SamplePlayer(ac, SampleManager.sample("Menu_Side_Beep.wav"));
+        SamplePlayer Sbeep = new SamplePlayer(ac, SampleManager.sample("resources/Menu_Side_Beep.wav"));
         ac.out.addInput(Sbeep);
     }
     
     public void centerBeep() {
-        SamplePlayer Mbeep = new SamplePlayer(ac, SampleManager.sample("Menu_Center_Beep.wav"));
+        SamplePlayer Mbeep = new SamplePlayer(ac, SampleManager.sample("resources/Menu_Center_Beep.wav"));
         ac.out.addInput(Mbeep);
     }
     
     public void fightSounds() {
         SamplePlayer fight;
         if(Math.random()*2 > 1) {
-           fight = new SamplePlayer(ac, SampleManager.sample("Fight_Sounds.wav"));
+           fight = new SamplePlayer(ac, SampleManager.sample("resources/Fight_Sounds.wav"));
         }
         else
-           fight = new SamplePlayer(ac, SampleManager.sample("Fight_Sounds_2.wav"));
+           fight = new SamplePlayer(ac, SampleManager.sample("resources/Fight_Sounds_2.wav"));
         ac.out.addInput(fight);
     }
     
     public void failSound() {
-        SamplePlayer fail = new SamplePlayer(ac, SampleManager.sample("Task_Time_Out.wav"));
+        SamplePlayer fail = new SamplePlayer(ac, SampleManager.sample("resources/Task_Time_Out.wav"));
         ac.out.addInput(fail);
     }
     
     public void failJingle() {
-        SamplePlayer jing =  new SamplePlayer(ac, SampleManager.sample("Stranded_GameOver"));
+        SamplePlayer jing =  new SamplePlayer(ac, SampleManager.sample("resources/Stranded_GameOver"));
         ac.out.clearInputConnections();
         ac.out.addInput(jing);
     }
